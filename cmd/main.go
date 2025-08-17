a758f5390ceca9b9ba44753ba1ab1dbeda7c9571
cbd <bojiuyixiao@foxmail.com>
2025-08-17 21:21:16 +0800

    feat: 添加 Swagger API 文档

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-    "github.com/Chabuduo04/go-todo-app/pkg/database"
-    "github.com/Chabuduo04/go-todo-app/internal/routes"
-    "github.com/gin-gonic/gin"
+	_ "github.com/Chabuduo04/go-todo-app/docs"
+	"github.com/Chabuduo04/go-todo-app/internal/routes"
+	"github.com/Chabuduo04/go-todo-app/pkg/database"
+	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Todo API
+// @version 1.0
+// @description 一个简易的任务管理系统，支持用户注册、登录和 Todo 管理
+// @host localhost:8080
+// @BasePath /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
-    database.InitDB()
+	database.InitDB()
 
-    r := gin.Default()
-    routes.SetupRoutes(r)
+	r := gin.Default()
+	routes.SetupRoutes(r)
 
-    r.Run(":8080") // 启动服务
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":8080") // 启动服务
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,21 +1,21 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/Chabuduo04/go-todo-app/internal/controllers"
-    "github.com/Chabuduo04/go-todo-app/internal/middleware"
+	"github.com/Chabuduo04/go-todo-app/internal/controllers"
+	"github.com/Chabuduo04/go-todo-app/internal/middleware"
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
-    r.POST("/register", controllers.Register)
-    r.POST("/login", controllers.Login)
+	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
 
-    auth := r.Group("/")
-    auth.Use(middleware.AuthMiddleware())
-    {
-        auth.GET("/todos", controllers.GetTodos)
-        auth.POST("/todos", controllers.CreateTodo)
-        auth.PUT("/todos/:id", controllers.UpdateTodo)
-        auth.DELETE("/todos/:id", controllers.DeleteTodo)
-    }
+	auth := r.Group("/")
+	auth.Use(middleware.AuthMiddleware())
+	{
+		auth.GET("/todos", controllers.GetTodos)
+		auth.POST("/todos", controllers.CreateTodo)
+		auth.PUT("/todos/:id", controllers.UpdateTodo)
+		auth.DELETE("/todos/:id", controllers.DeleteTodo)
+	}
 }
